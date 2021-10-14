67188c1c51491909b90c56a1f0b73cce066b0d9a
Maarij Fatima <59571613+maarij21@users.noreply.github.com>
2021-10-13 22:48:46 -0400

    added src files

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import React from 'react';
+import Row from './Row';
+import requests from './requests';
+import Banner from './Banner.js';
+import Nav from './Nav.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Banner />
+      <Row title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals} 
+      isLargeRow/>
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+    </div>
+  );
+}
+
+export default App;
