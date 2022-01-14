1f2eb041ca29eaa5fb4ac62f127c629b922c1331
Maarij Fatima <59571613+maarij21@users.noreply.github.com>
2022-01-14 11:42:13 -0500

    added comment
    added comment

--- src/axios.js
+++ src/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
+/* create an instance and using the axios method .create to get the base url to make requests to the movie database */
 const instance = axios.create({
     baseURL: "https://api.themoviedb.org/3",
 });
